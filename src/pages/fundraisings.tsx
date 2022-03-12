778827f6d4c5d4e7f41d7cb1df9027614125a72d
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2022-03-12 14:17:04 +0100

    Number of funds

--- src/pages/fundraisings.tsx
+++ src/pages/fundraisings.tsx
@@ -1,4 +1,4 @@
-import { Divider, Heading, SimpleGrid } from "@chakra-ui/react";
+import { Divider, Heading, SimpleGrid, Text } from "@chakra-ui/react";
 
 import { FundraisingCard } from "lib/components/FundraisingCard";
 
@@ -13,8 +13,11 @@ const property = {
 const Fundraisings = () => {
   return (
     <>
-      <Heading as="h1" fontSize={{ base: "lg", sm: "3xl" }}>
+      <Heading as="h1" fontWeight="bold" fontSize={{ base: "lg", sm: "3xl" }}>
         Fundraisings
+        <Text marginLeft={2} as="span" color="gray.300">
+          (12)
+        </Text>
       </Heading>
       <Divider marginTop={10} />
       <SimpleGrid marginTop={10} columns={[1, 2, 3, 4]} spacing="24px">
