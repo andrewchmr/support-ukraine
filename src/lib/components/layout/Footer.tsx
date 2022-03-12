304deada6db9c472b97c1f790518d8b1ff9c2ce9
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2022-03-12 23:43:02 +0100

    Footer update

--- src/lib/components/layout/Footer.tsx
+++ src/lib/components/layout/Footer.tsx
@@ -1,29 +1,49 @@
 import { Box, Text } from "@chakra-ui/react";
 
 import { Crest } from "../icons/Crest";
+import { Facebook } from "../icons/Facebook";
+import { Telegram } from "../icons/Telegram";
 
 import { LayoutContainer } from "./LayoutContainer";
 
 const Footer = () => {
   return (
-    <Box as="footer" marginTop="auto">
-      <Box py="2" backgroundColor="#3693C2" />
-      <LayoutContainer width="full" py="12">
-        <Text fontWeight="bold" fontSize="xl">
-          Contact Us
-        </Text>
-      </LayoutContainer>
-      <Box
+    <Box
+      marginTop="auto"
+      as="footer"
+      alignItems="center"
+      backgroundColor="#F6CC25"
+      py="6"
+    >
+      <LayoutContainer
         display="flex"
-        width="full"
-        justifyContent="center"
         alignItems="center"
-        backgroundColor="#F6CC25"
-        py="6"
+        justifyContent="space-between"
       >
-        <Crest />
-        <Text marginLeft={4}>Ukraine {new Date().getFullYear()}</Text>
-      </Box>
+        <Box display="flex" alignItems="center">
+          <Box gap="2" display="flex" marginRight={10}>
+            <Facebook />
+            <Telegram />
+          </Box>
+
+          <Box display="flex" alignItems="center">
+            <Text
+              whiteSpace="nowrap"
+              fontWeight="bold"
+              fontSize="xl"
+              marginRight={5}
+            >
+              Contact Us
+            </Text>
+            example@gmail.com
+          </Box>
+        </Box>
+
+        <Box display="flex" alignItems="center">
+          <Crest />
+          <Text marginLeft={4}>Ukraine {new Date().getFullYear()}</Text>
+        </Box>
+      </LayoutContainer>
     </Box>
   );
 };
