030303f8fda136ed710a248ddc9368a96ff6eb9b
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2022-03-12 18:32:42 +0100

    Mark active page

--- src/lib/components/layout/Header.tsx
+++ src/lib/components/layout/Header.tsx
@@ -1,15 +1,33 @@
-import { Box, Flex, Heading } from "@chakra-ui/react";
-import Link from "next/link";
+import { Box, Flex, Heading, Link } from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
+
+const links = [{ name: "Fundraisings", path: "/fundraisings" }];
 
 const Header = () => {
+  const router = useRouter();
+
   return (
     <Flex as="header" width="full" align="center">
       <Heading as="h1" size="md">
         <Link href="/">Logo</Link>
       </Heading>
-
       <Box marginLeft="auto">
-        <Link href="/fundraisings">Fundraisings</Link>
+        {links.map((link) => {
+          const isActive = router.pathname === link.path;
+
+          return (
+            <NextLink key={link.path} href={link.path} passHref>
+              <Link
+                fontSize="lg"
+                fontWeight={isActive ? "semibold" : undefined}
+                textDecoration={isActive ? "underline" : undefined}
+              >
+                {link.name}
+              </Link>
+            </NextLink>
+          );
+        })}
       </Box>
     </Flex>
   );
