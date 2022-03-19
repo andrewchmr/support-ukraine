7b5eab9538543dde0a36e8da7d085c61213da6ab
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2022-03-19 21:48:25 +0100

    Analytics (#25)
    * Analytics
    
    * Extend window type
    
    * Update env

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,31 @@
-/* eslint-disable react/jsx-props-no-spreading */
 import { Box, ChakraProvider } from "@chakra-ui/react";
 import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import { Router } from "next/router";
+import Script from "next/script";
+import React from "react";
 
 import defaultSEOConfig from "../../next-seo.config";
 import customTheme from "lib/styles/customTheme";
 import "lib/styles/globals.css";
+import { GA_TRACKING_ID, pageview } from "utils/gtag";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
+  const handleGoogleAnalytics = () => {
+    const handleRouteChange = (url: string) => {
+      pageview(url);
+    };
+    Router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      Router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  };
+
+  React.useEffect(() => {
+    handleGoogleAnalytics();
+  }, []);
+
   return (
     <ChakraProvider theme={customTheme}>
       <Head>
@@ -18,6 +35,22 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
         />
       </Head>
       <DefaultSeo {...defaultSEOConfig} />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="googleTagManager"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       <Box display="flex" flexDirection="column" height="100vh" margin="0">
         <Component {...pageProps} />
       </Box>
