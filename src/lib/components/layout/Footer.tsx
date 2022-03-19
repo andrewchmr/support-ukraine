39cb173b8c7c245f5f3dda7cdbc4963da09c9f2f
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2022-03-19 12:03:49 +0100

    feat: Footer (#22)
    * Responsive footer
    
    * Add email icon

--- src/lib/components/layout/Footer.tsx
+++ src/lib/components/layout/Footer.tsx
@@ -2,6 +2,7 @@ import { Box, Text } from "@chakra-ui/react";
 
 import { Crest } from "../icons/Crest";
 import { Facebook } from "../icons/Facebook";
+import { MailCircle } from "../icons/MailCircle";
 import { Telegram } from "../icons/Telegram";
 
 import { LayoutContainer } from "./LayoutContainer";
@@ -19,14 +20,29 @@ const Footer = () => {
         display="flex"
         alignItems="center"
         justifyContent="space-between"
+        flexDirection={{ base: "column", md: "row" }}
       >
-        <Box id="contact" display="flex" alignItems="center">
-          <Box gap="2" display="flex" marginRight={10}>
+        <Box
+          id="contact"
+          display="flex"
+          alignItems={["start", "center"]}
+          width={["100%", "initial"]}
+          justifyContent={["space-between", "center"]}
+          flexDirection={["row-reverse", "row"]}
+        >
+          <Box gap="2" display="flex" marginRight={["unset", 10]}>
             <Facebook />
             <Telegram />
+            <Box as="a" href="mailto:contact@ukraine-army.help">
+              <MailCircle />
+            </Box>
           </Box>
 
-          <Box display="flex" alignItems="center">
+          <Box
+            display="flex"
+            alignItems={["start", "center"]}
+            flexDirection={["column", "row"]}
+          >
             <Text
               whiteSpace="nowrap"
               fontWeight="bold"
@@ -35,11 +51,15 @@ const Footer = () => {
             >
               Contact Us
             </Text>
-            example@gmail.com
+            contact@ukraine-army.help
           </Box>
         </Box>
 
-        <Box display="flex" alignItems="center">
+        <Box
+          display="flex"
+          alignItems="center"
+          marginTop={{ base: "6", md: "unset" }}
+        >
           <Crest />
           <Text marginLeft={4}>Ukraine {new Date().getFullYear()}</Text>
         </Box>
