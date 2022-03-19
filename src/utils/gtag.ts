0befcbcece8cae96f7740a90bbe2ffbc1c66cb3d
Andriy Chemerynskiy <andrzej.chem@gmail.com>
2022-03-19 19:04:26 +0100

    Update env

--- src/utils/gtag.ts
+++ src/utils/gtag.ts
@@ -1,7 +1,4 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-require("dotenv").config();
-
-export const { GA_TRACKING_ID } = process.env;
+export const GA_TRACKING_ID = "G-KLK9DBYJ8X";
 
 export const pageview = (url: string) => {
   window.gtag("config", process.env.GA_TRACKING_ID, {
